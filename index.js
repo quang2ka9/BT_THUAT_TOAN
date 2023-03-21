f980623163585d45c0da5f9109528a1250102691
quang <ltb01628645546@gmail.com>
2023-03-21 17:39:38 +0700

    done

---
+++ index.js
@@ -0,0 +1,32 @@
+//1. Kiểm tra xem mảng có tăng dần hay không
+//
+// let a = () => {
+//     let arr = [1,2,3,4]
+//     for (let i = 0; i < arr.length-1; i++) {
+//         if (arr[i] >= arr[i + 1]) {
+//             return false
+//         }
+//     }
+//     return true
+// }
+//
+// console.log(a())
+
+
+// 2. Hãy đếm giá trị trong mảng thỏa mãn: lớn hơn tất cả các giá trị đứng đằng trước nó
+//
+// let arr = [1, 3, 2, 4, 3, 5, 2, 6];
+//     let count = 0;
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[i] > arr[i-1]) {
+//             count++;
+//         }
+//     }
+// console.log(count);
+
+
+
+
+
+
+
